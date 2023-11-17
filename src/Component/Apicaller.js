e8aec9973abf886a38d951608cd0e6be948e0e0c
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2023-11-17 20:19:59 +0530

    get Api call and display data with status message

--- src/Component/Apicaller.js
+++ src/Component/Apicaller.js
@@ -1,8 +1,23 @@
 import React, { useEffect, useState } from 'react'
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 
 export default function ApiCaller() {
   const [data, setData] = useState([])
+  // const [value,setValue] = useState({})
+  const [pname,setPname] =useState('')
+  const [price, setPrice] =useState('')
+
+  const [status, setStatus]=useState('')
+
+  const pNamehandler=(event)=>{
+    setPname(event.target.value)
+    console.log(pname);
+  }
+
+  const pricehandler=(event)=>{
+    setPrice(event.target.value)
+    console.log(price);
+  }
 
   useEffect(() => {
     console.log("hi");
@@ -24,17 +39,48 @@ export default function ApiCaller() {
 
       .catch(error => {
         console.error('There was a problem with the fetch operation:', error);
+
       });
   }, [])
 
   const handlerApi = () => {
+    console.log(pname , price);
+
+    // var mypname="";
+    // var myprice= "";
+    fetch(`http://surevih.in/api/saveproduct?productname=${pname}&price=${price}`)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Network response was not ok.');
+        }
+      })
+      .then(result => {
+        console.log(result['status']);
+        // console.log(result);
+        // console.log(result['book'][0])
+        var singledata = result['book'];
+        // setData(result);
+        setData(singledata);
+        setStatus(result['status'])
+      })
+
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+        setStatus("Errors")
+      });
 
   }
-  console.warn("Data : ", data);
+  // console.warn("Data : ", data);
   return (
     <Box>
       <Typography variant="h2" >Welcome</Typography>
-      {
+      <TextField variant='outlined' label='Product Name' id='pname' value={pname} onChange={pNamehandler}></TextField>
+      <TextField variant='outlined' label='Price' id='price' type='number' onChange={pricehandler} ></TextField>
+      
+    
+      {/* {
         data.map((item) =>
           <Box width={500} style={{ border: '2px solid black' }} textAlign='start' padding={5} my={2} >
             <Typography variant="h6" py={1}  >{item.id}</Typography>
@@ -42,10 +88,12 @@ export default function ApiCaller() {
             <Typography variant="h6" py={1} >{item.price}</Typography>
             <Typography variant="h6" py={1} >{item.published}</Typography>
           </Box>
+          
         )
-      }
+      } */}
 
       <Button variant="contained" onClick={handlerApi}>Save</Button>
+      <Typography variant="h2" >{status}</Typography>
     </Box>
   );
 }
