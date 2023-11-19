2511ec9ddd1295740d00386ae539b701d342a8b2
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2023-11-19 22:29:26 +0530

    customize sidebar(md & xs)

--- src/Component/Homecontent1.js
+++ src/Component/Homecontent1.js
@@ -8,17 +8,15 @@ export default function HomeContent1() {
     return (
         <Box paddingTop={5}>
             <Typography paddingBottom={5} variant='h2'>Out Top Product</Typography>
-
             <Grid container >
 
                 {/* Sidebar */}
-                <Grid xs={4} md={5} sx={{ p: 2 , backgroundColor: '#bdbdbd'}}>
+                <Grid xs={4} md={3} sx={{ p: 2 , backgroundColor: '#bdbdbd'}}>
                     <Sidebars />
-
                 </Grid>
 
                 {/* Content */}
-                <Grid xs={8} md={7} style={{ border: '2px solid red', backgroundColor: 'yellow' }} padding={2} >
+                <Grid xs={8} md={9} style={{ border: '2px solid red', backgroundColor: 'yellow' }} padding={2} >
                     <Typography variant='h4'>Product Container</Typography>
                     <Grid container style={{ border: '2px solid green', backgroundColor: 'white' }} p={1}   >
                         <Grid xs={6} md={4}  >
